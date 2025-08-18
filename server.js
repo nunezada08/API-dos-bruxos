369497bcb4e8d3ec4742caaefb559d7b254f7ffd
nunezada08 <vitor.nunes3013@gmail.com>
2025-08-18 14:57:22 -0300

    Adiciona arquivos para levantar servidor

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bruxos from "./src/data/bruxos.js";
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("Vamos de Harry Potter")
+})
+
+app.get("/bruxos", (req, res) => {
+    res.json(bruxos);
+});
+
+app.listen(3000, () => {
+    console.log("🧙‍♂️ API dos Bruxos está no ar na porta 3000 !");
+});
