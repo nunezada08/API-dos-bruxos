a59691e731a1b828f7d49e258dae8f67c0ec2c60
nunezada08 <vitor.nunes3013@gmail.com>
2025-08-25 08:25:03 -0300

    Feat: adiciona o get by id e o get by name.

--- server.js
+++ server.js
@@ -11,6 +11,36 @@ app.get("/bruxos", (req, res) => {
   res.json(bruxos);
 });
 
+app.get("/bruxos/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const bruxo = bruxos.find(b => b.id === id);
+
+    if(bruxo) {
+        res.status(200).json(bruxo);
+    } else {
+        res.status(404).json({
+            mensagem: "Bruxo não encontrado"
+        })
+    }
+});
+
+app.get("/personagem/nome/:nome", (req, res) => {
+    let nome = req.params.nome
+    nome = nome.toLowerCase()
+
+    const nomeFiltrados = bruxos.filter(b => b.nome.toLowerCase().includes(nome));
+
+    if(nomeFiltrados) {
+        res.status(200).json(nomeFiltrados);
+    } else {
+        res.status(404).json({
+            mensagem: "Bruxo não encontrado"
+        })
+    }
+});
+
 app.listen(3000, () => {
   console.log("🧙‍♂️ API dos Bruxos está no ar na porta 3000 !");
 });
+
